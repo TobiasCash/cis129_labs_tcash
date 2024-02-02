300686e469ee5fcf135dc3ef3eab46ae1d13776f
TobiasCash <158229678+TobiasCash@users.noreply.github.com>
2024-02-01 22:41:57 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Programmer: Tobias Cash
+# deleted the Hello World print() statement
+# added multiple print() statements which tell a story
+
+# This program uses the print() statement to print a Story. 
+print('This short story is about my adventure with learning Python')
+print('Python is one of the most popular high-level programming language')
+print('It is used for object oriented programming and task automation.')
+print('How awesome!')
